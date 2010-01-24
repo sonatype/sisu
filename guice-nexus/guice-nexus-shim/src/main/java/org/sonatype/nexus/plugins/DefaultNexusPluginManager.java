7aa3de0b8c28c5e1f0df032f13ae8f44705b45c1
Stuart McCulloch <mcculls@sonatype.com>
2010-01-24 14:35:53 +0000

    OSGI-135: minimal scaffolding

---
+++ guice-nexus/guice-nexus-shim/src/main/java/org/sonatype/nexus/plugins/DefaultNexusPluginManager.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2009 Sonatype, Inc. All rights reserved.
+ *
+ * This program is licensed to you under the Apache License Version 2.0,
+ * and you may not use this file except in compliance with the Apache License Version 2.0.
+ * You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the Apache License Version 2.0 is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+ */
+package org.sonatype.nexus.plugins;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+
+import org.sonatype.plugin.metadata.GAVCoordinate;
+
+public final class DefaultNexusPluginManager
+{
+    public Collection<PluginManagerResponse> activateInstalledPlugins()
+    {
+        return Collections.emptyList();
+    }
+
+    public Map<GAVCoordinate, PluginResponse> getPluginResponses()
+    {
+        return Collections.emptyMap();
+    }
+}
--- guice-nexus/guice-nexus-shim/src/main/java/org/sonatype/nexus/plugins/a.java
+++
@@ -1,5 +0,0 @@
-package org.sonatype.nexus.plugins;
-
-public interface a
-{
-}
