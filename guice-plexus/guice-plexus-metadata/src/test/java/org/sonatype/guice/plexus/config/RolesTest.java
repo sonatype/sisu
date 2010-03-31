232e4bfcbf5c036b7f89821419971fcf05968fcd
Stuart McCulloch <mcculls@sonatype.com>
2010-03-31 16:05:36 +0000

    Add UT for refactored utility method

--- guice-plexus/guice-plexus-metadata/src/test/java/org/sonatype/guice/plexus/config/RolesTest.java
+++ guice-plexus/guice-plexus-metadata/src/test/java/org/sonatype/guice/plexus/config/RolesTest.java
@@ -104,4 +104,12 @@ public class RolesTest
         {
         }
     }
+
+    public void testCamelization()
+    {
+        assertEquals( "thisIsATest", Roles.camelizeName( "this-is-a-test" ) );
+        assertEquals( "TestingA", Roles.camelizeName( "-testing-a" ) );
+        assertEquals( "testingB", Roles.camelizeName( "testing-b-" ) );
+        assertEquals( "TestingC", Roles.camelizeName( "--testing--c--" ) );
+    }
 }
