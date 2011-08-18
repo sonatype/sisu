ffbc6f8708476d72c861fa9714a7ddea128f684c
Stuart McCulloch <mcculls@gmail.com>
2011-08-18 07:06:12 +0100

    Simplify visible realms cache

--- sisu-inject/containers/guice-plexus/guice-plexus-locators/src/main/java/org/sonatype/guice/plexus/locators/ClassRealmUtils.java
+++ sisu-inject/containers/guice-plexus/guice-plexus-locators/src/main/java/org/sonatype/guice/plexus/locators/ClassRealmUtils.java
@@ -8,15 +8,14 @@
 package org.sonatype.guice.plexus.locators;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.WeakHashMap;
 
 import org.codehaus.plexus.classworlds.realm.ClassRealm;
-
-import com.google.common.cache.Cache;
-import com.google.common.cache.CacheBuilder;
-import com.google.common.cache.CacheLoader;
 
 public final class ClassRealmUtils
 {
@@ -58,8 +57,8 @@ public final class ClassRealmUtils
     // Implementation fields
     // ----------------------------------------------------------------------
 
-    private static Cache<ClassRealm, Set<String>> visibleRealmNameCache =
-        CacheBuilder.newBuilder().weakKeys().build( new VisibleRealmNames() );
+    private static Map<ClassRealm, Set<String>> namesCache =
+        Collections.synchronizedMap( new WeakHashMap<ClassRealm, Set<String>>() );
 
     // ----------------------------------------------------------------------
     // Utility methods
@@ -81,40 +80,40 @@ public final class ClassRealmUtils
     {
         if ( GET_IMPORT_REALMS_SUPPORTED && null != contextRealm )
         {
-            return visibleRealmNameCache.getUnchecked( contextRealm );
+            Set<String> names = namesCache.get( contextRealm );
+            if ( null == names )
+            {
+                namesCache.put( contextRealm, names = computeVisibleNames( contextRealm ) );
+            }
+            return names;
         }
         return null;
     }
 
     // ----------------------------------------------------------------------
-    // Implementation types
+    // Implementation methods
     // ----------------------------------------------------------------------
 
-    static final class VisibleRealmNames
-        extends CacheLoader<ClassRealm, Set<String>>
+    @SuppressWarnings( "unchecked" )
+    private static Set<String> computeVisibleNames( final ClassRealm forRealm )
     {
-        @Override
-        @SuppressWarnings( "unchecked" )
-        public Set<String> load( final ClassRealm forRealm )
-        {
-            final Set<String> visibleRealmNames = new HashSet<String>();
-            final List<ClassRealm> searchRealms = new ArrayList<ClassRealm>();
+        final Set<String> visibleRealmNames = new HashSet<String>();
+        final List<ClassRealm> searchRealms = new ArrayList<ClassRealm>();
 
-            searchRealms.add( forRealm );
-            for ( int i = 0; i < searchRealms.size(); i++ )
+        searchRealms.add( forRealm );
+        for ( int i = 0; i < searchRealms.size(); i++ )
+        {
+            final ClassRealm realm = searchRealms.get( i );
+            if ( visibleRealmNames.add( realm.toString() ) )
             {
-                final ClassRealm realm = searchRealms.get( i );
-                if ( visibleRealmNames.add( realm.toString() ) )
+                searchRealms.addAll( realm.getImportRealms() );
+                final ClassRealm parent = realm.getParentRealm();
+                if ( null != parent )
                 {
-                    searchRealms.addAll( realm.getImportRealms() );
-                    final ClassRealm parent = realm.getParentRealm();
-                    if ( null != parent )
-                    {
-                        searchRealms.add( parent );
-                    }
+                    searchRealms.add( parent );
                 }
             }
-            return visibleRealmNames;
         }
+        return visibleRealmNames;
     }
 }
