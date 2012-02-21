057d630a237282b46524715a1075fac48cf3c684
Stuart McCulloch <mcculls@gmail.com>
2012-02-21 02:29:36 +0000

    Fix copyright headers

--- org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/BeanScanning.java
+++ org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/BeanScanning.java
@@ -4,13 +4,13 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * and Apache License v2.0 which accompanies this distribution.
  * The Eclipse Public License is available at
- *   http://www.eclipse.org/legal/epl-v10.html
+ *     http://www.eclipse.org/legal/epl-v10.html
  * The Apache License v2.0 is available at
- *   http://www.apache.org/licenses/LICENSE-2.0.html
+ *     http://www.apache.org/licenses/LICENSE-2.0.html
  * You may elect to redistribute this code under either of these licenses.
  *
  * Contributors:
- *   Sonatype, Inc. - initial API and implementation
+ *     Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
 package org.eclipse.sisu;
 
