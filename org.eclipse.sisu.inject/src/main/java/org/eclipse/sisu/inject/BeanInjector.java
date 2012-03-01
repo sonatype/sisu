572f44dab67845ae416e24d0bc924e120ba86ca9
Stuart McCulloch <mcculls@gmail.com>
2012-03-01 20:58:33 +0000

    Contribution should be EPL-only as per discussion on https://dev.eclipse.org/ipzilla/show_bug.cgi?id=6268

--- org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/inject/BeanInjector.java
+++ org.eclipse.sisu.inject/src/main/java/org/eclipse/sisu/inject/BeanInjector.java
@@ -2,15 +2,11 @@
  * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
- * and Apache License v2.0 which accompanies this distribution.
- * The Eclipse Public License is available at
- *     http://www.eclipse.org/legal/epl-v10.html
- * The Apache License v2.0 is available at
- *     http://www.apache.org/licenses/LICENSE-2.0.html
- * You may elect to redistribute this code under either of these licenses.
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Sonatype, Inc. - initial API and implementation
+ *    Sonatype, Inc. - initial API and implementation
  *******************************************************************************/
 package org.eclipse.sisu.inject;
 
