d9570f5d1a6d9dccb0ad8dadff48539a22ec5a95
Stuart McCulloch <mcculls@gmail.com>
2012-07-22 01:40:24 +0100

    Cleanup assembly

--- sisu-inject/containers/org.eclipse.sisu.plexus/src/main/java/org/eclipse/sisu/plexus/shim/PlexusSpaceModule.java
+++ sisu-inject/containers/org.eclipse.sisu.plexus/src/main/java/org/eclipse/sisu/plexus/shim/PlexusSpaceModule.java
@@ -5,7 +5,7 @@
  * which accompanies this distribution, and is available at
  *   http://www.eclipse.org/legal/epl-v10.html
  *******************************************************************************/
-package org.sonatype.guice.plexus.shim;
+package org.eclipse.sisu.plexus.shim;
 
 import java.util.Map;
 
