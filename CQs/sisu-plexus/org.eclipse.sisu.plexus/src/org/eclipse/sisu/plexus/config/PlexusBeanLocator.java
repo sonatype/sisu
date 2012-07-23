f073d7d0df2a58e695260692159e7336edd382e8
Stuart McCulloch <mcculls@gmail.com>
2012-07-23 01:02:51 +0100

    Fix copyrights

--- CQs/sisu-plexus/org.eclipse.sisu.plexus/src/org/eclipse/sisu/plexus/config/PlexusBeanLocator.java
+++ CQs/sisu-plexus/org.eclipse.sisu.plexus/src/org/eclipse/sisu/plexus/config/PlexusBeanLocator.java
@@ -1,9 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2010-2011 Sonatype, Inc.
+ * Copyright (c) 2010, 2012 Sonatype, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
- *   http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
  *******************************************************************************/
 package org.eclipse.sisu.plexus.config;
 
