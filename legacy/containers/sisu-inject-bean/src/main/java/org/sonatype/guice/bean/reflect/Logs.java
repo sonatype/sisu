fd2282da56d53c5f01b27b411e66081cf1745499
Stuart McCulloch <mcculls@gmail.com>
2013-05-27 18:41:56 +0100

    Reflect wrappers

---
+++ legacy/containers/sisu-inject-bean/src/main/java/org/sonatype/guice/bean/reflect/Logs.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.sonatype.guice.bean.reflect;
+
+import com.google.inject.Injector;
+import com.google.inject.Module;
+
+@Deprecated
+public final class Logs
+{
+    private Logs()
+    {
+    }
+
+    public static void debug( final String format, final Object arg1, final Object arg2 )
+    {
+        org.eclipse.sisu.inject.Logs.trace( format, arg1, arg2 );
+    }
+
+    public static void warn( final String format, final Object arg1, final Object arg2 )
+    {
+        org.eclipse.sisu.inject.Logs.warn( format, arg1, arg2 );
+    }
+
+    public static void catchThrowable( final Throwable problem )
+    {
+        org.eclipse.sisu.inject.Logs.catchThrowable( problem );
+    }
+
+    public static void throwUnchecked( final Throwable problem )
+    {
+        org.eclipse.sisu.inject.Logs.throwUnchecked( problem );
+    }
+
+    public static String identityToString( final Object object )
+    {
+        return org.eclipse.sisu.inject.Logs.identityToString( object );
+    }
+
+    public static String toString( final Module module )
+    {
+        return org.eclipse.sisu.inject.Logs.toString( module );
+    }
+
+    public static String toString( final Injector injector )
+    {
+        return org.eclipse.sisu.inject.Logs.toString( injector );
+    }
+}
---
+++ legacy/containers/sisu-inject-bean/src/main/java/org/sonatype/guice/bean/reflect/Weak.java
@@ -0,0 +1,73 @@
+/*******************************************************************************
+ * Copyright (c) 2010, 2013 Sonatype, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stuart McCulloch (Sonatype, Inc.) - initial API and implementation
+ *******************************************************************************/
+package org.sonatype.guice.bean.reflect;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+
+@Deprecated
+public final class Weak
+{
+    private Weak()
+    {
+    }
+
+    public static <T> Collection<T> elements()
+    {
+        return org.eclipse.sisu.inject.Weak.elements();
+    }
+
+    public static <T> Collection<T> elements( final int capacity )
+    {
+        return org.eclipse.sisu.inject.Weak.elements( capacity );
+    }
+
+    public static <K, V> Map<K, V> keys()
+    {
+        return org.eclipse.sisu.inject.Weak.keys();
+    }
+
+    public static <K, V> Map<K, V> keys( final int capacity )
+    {
+        return org.eclipse.sisu.inject.Weak.keys( capacity );
+    }
+
+    public static <K, V> ConcurrentMap<K, V> concurrentKeys()
+    {
+        return org.eclipse.sisu.inject.Weak.concurrentKeys();
+    }
+
+    public static <K, V> ConcurrentMap<K, V> concurrentKeys( final int capacity, final int concurrency )
+    {
+        return org.eclipse.sisu.inject.Weak.concurrentKeys( capacity, concurrency );
+    }
+
+    public static <K, V> Map<K, V> values()
+    {
+        return org.eclipse.sisu.inject.Weak.values();
+    }
+
+    public static <K, V> Map<K, V> values( final int capacity )
+    {
+        return org.eclipse.sisu.inject.Weak.values( capacity );
+    }
+
+    public static <K, V> ConcurrentMap<K, V> concurrentValues()
+    {
+        return org.eclipse.sisu.inject.Weak.concurrentValues();
+    }
+
+    public static <K, V> ConcurrentMap<K, V> concurrentValues( final int capacity, final int concurrency )
+    {
+        return org.eclipse.sisu.inject.Weak.concurrentValues( capacity, concurrency );
+    }
+}
