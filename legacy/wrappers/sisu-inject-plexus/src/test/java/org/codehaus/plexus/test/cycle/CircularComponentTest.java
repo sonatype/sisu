8364821df514de9eb52e6615eff9f15713d284e2
Stuart McCulloch <mcculls@gmail.com>
2016-02-07 17:52:00 +0000

    Extend CircularComponentTest to cover scenario described in https://bugs.eclipse.org/bugs/show_bug.cgi?id=487090

--- legacy/wrappers/sisu-inject-plexus/src/test/java/org/codehaus/plexus/test/cycle/CircularComponentTest.java
+++ legacy/wrappers/sisu-inject-plexus/src/test/java/org/codehaus/plexus/test/cycle/CircularComponentTest.java
@@ -1,6 +1,10 @@
 package org.codehaus.plexus.test.cycle;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.codehaus.plexus.PlexusTestCase;
+
+import junit.framework.Assert;
 
 public class CircularComponentTest
     extends PlexusTestCase
@@ -9,5 +13,53 @@ public class CircularComponentTest
         throws Exception
     {
         lookup( CycleComponent.class, "A" );
+    }
+
+    public void testCircularComponentsInParallel()
+        throws Exception
+    {
+        AtomicInteger passCount = new AtomicInteger();
+
+        // access same cycle in parallel; activation of the cycle should be rooted
+        // in the first scoped component rather than the first requested component
+        // and should only be visible to other threads after activation
+
+        Thread t1 = new Thread( new ParallelLookup( passCount ) );
+        Thread t2 = new Thread( new ParallelLookup( passCount ) );
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        Assert.assertEquals( 2, passCount.get() );
+    }
+
+    private class ParallelLookup
+        implements Runnable
+    {
+        AtomicInteger passCount;
+
+        public ParallelLookup( AtomicInteger passCount )
+        {
+            this.passCount = passCount;
+        }
+
+        public void run()
+        {
+            try
+            {
+                // accesses the same cycle indirectly from an unscoped component
+                CycleComponent cycle = lookup( CycleComponent.class, "parallel" );
+                // assertion will be thrown if cycle is not activated before use
+                cycle.next().next().next();
+                passCount.incrementAndGet();
+            }
+            catch ( final Exception e )
+            {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- legacy/wrappers/sisu-inject-plexus/src/test/java/org/codehaus/plexus/test/cycle/DefaultCycleComponentC.java
+++ legacy/wrappers/sisu-inject-plexus/src/test/java/org/codehaus/plexus/test/cycle/DefaultCycleComponentC.java
@@ -1,6 +1,7 @@
 package org.codehaus.plexus.test.cycle;
 
 import org.codehaus.plexus.personality.plexus.lifecycle.phase.Initializable;
+import org.codehaus.plexus.personality.plexus.lifecycle.phase.InitializationException;
 
 import junit.framework.Assert;
 
@@ -9,13 +10,29 @@ public class DefaultCycleComponentC
 {
     CycleComponent d;
 
+    boolean initialized;
+
     public CycleComponent next()
     {
+        Assert.assertTrue( initialized ); // verify initialized before use
+
         return d;
     }
 
     public void initialize()
+        throws InitializationException
     {
-        Assert.assertNotNull( next().next().next() );
+        try
+        {
+            Thread.sleep( 500 ); // simulate some non-trivial initialization
+        }
+        catch ( InterruptedException e )
+        {
+            throw new InitializationException( e.getMessage(), e );
+        }
+
+        initialized = true;
+
+        Assert.assertNotNull( next().next().next() ); // exercise the cycle
     }
 }
